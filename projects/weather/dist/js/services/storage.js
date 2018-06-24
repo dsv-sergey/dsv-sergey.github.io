0b85f2299a8b4ad6985234080c20bace896c2f9e
dsv-sergey <dsv-sergey@mail.ru>
2018-06-24 23:55:36 +0300

    weather config with gulp

---
+++ projects/weather/dist/js/services/storage.js
@@ -0,0 +1,13 @@
+var storage = (function(){
+    return {
+        set: function(text) {
+
+        },
+        get: function(id) {
+
+        },
+        delete: function(id) {
+
+        },
+    };
+})();
