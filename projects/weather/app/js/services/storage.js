80e8e17bdc3fbb868cd163173f5a65cc8875733e
dsv-sergey <dsv-sergey@mail.ru>
2018-06-27 07:42:32 +0300

    add readme

--- projects/weather/app/js/services/storage.js
+++ projects/weather/app/js/services/storage.js
@@ -1,13 +1,67 @@
 var storage = (function(){
+    var databaseURL = 'https://dsv-sergey.firebaseio.com/progects/weather';
+
     return {
-        set: function(text) {
+        set: function(type, text, methodSend) {
+            if (methodSend == 'XHR') {
 
+            } else if (methodSend == 'fetch') {
+                fetch(config.databaseURL + '/messages.json', {
+                    method: 'PUT',
+                    body: JSON.stringify([
+                      {
+                        user: from,
+                        message: text,
+                        date: date,
+                        title: counter++ + 'message',
+                      },
+                    ]),
+                    headers: {
+                      Accept: 'application/json',
+                      'Content-Type': 'application/json',
+                    },
+                  })
+                    .then(function(response) {
+                      return response.json();
+                    })
+                    .then(console.log);
+            }
         },
-        get: function(id) {
+        getNote: function(type, id, methodSend) {
+            if (methodSend == 'XHR') {
 
+            } else if (methodSend == 'fetch') {
+                fetch(config.databaseURL + '/messages.json', {
+                    headers: {
+                      Accept: 'application/json',
+                      'Content-Type': 'application/json',
+                    },
+                  })
+                    .then(function(response) {
+                      return response.json();
+                    })
+                    .then(console.log);
+            }
         },
-        delete: function(id) {
+        getAllNotes: function(type, id, methodSend) {
+            fetch(config.databaseURL + '/.json', {
+                headers: {
+                  Accept: 'application/json',
+                  'Content-Type': 'application/json',
+                },
+              })
+                .then(function(response) {
+                  return response.json();
+                })
+                .then(console.log);
+        },
 
+        delete: function(type, id, method) {
+            if (method == 'XHR') {
+
+            } else if (method == 'fetch') {
+                
+            }
         },
     };
 })();
